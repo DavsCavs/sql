10f1c779b113024fd717819ed3c250084e0237c0
Davis <your@email.example>
2024-09-06 09:22:35 +0300

    added select distinct + added new custom field + select for certaint fields

---
+++ select/2.sql
@@ -0,0 +1,18 @@
+-- uzdevums- atlasit datus no tabulas 'customers'
+-- datubaze 'sql store'
+-- atlasit vardu, uzvardu un punktus
+-- papilduzdevums - atlasit papildus kolonu
+
+
+
+-- USE sql_store;
+-- SELECT first_name,
+ -- last_name,
+--  points,
+ -- points+15 as "points increased" 
+ -- FROM customers
+-- ORDER BY first_name;
+
+
+SELECT DISTINCT state FROM customers;
+
