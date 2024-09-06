73d5592b7b2fa9795b9c2cce1da13da1b5ee3664
Davis <your@email.example>
2024-09-06 10:36:12 +0300

    select with 'LIKE'

---
+++ select/5.sql
@@ -0,0 +1,6 @@
+-- USE sql_store;
+
+SELECT * FROM customers
+-- WHERE last_name LIKE 'B%'  mekle pec pirmo burtu un parejie var but vienalga ka 
+-- WHERE last_name LIKE 'B_________' mekle pec noteikta skaita pec pirma burta
+-- WHERE last_name LIKE '%B%' mekle ja varda ir "b" burta var atrasties jebkur
