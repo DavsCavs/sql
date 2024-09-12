4e16b43114336451597c5b13fb6ee494548169f3
Davis <your@email.example>
2024-09-12 09:23:06 +0300

    join orders and customers   ON vs USING

--- 2select-join/2.sql
+++ 2select-join/2.sql
@@ -12,3 +12,18 @@ WHERE reports_to IS null;
 USE sql_store;
 SELECT order_id, customer_id, status, name FROM orders
 JOIN order_statuses ON orders.status = order_statuses.order_status_id;
+
+
+
+USE sql_store;
+SELECT o.customer_id, o.order_id, c.first_name, c.last_name FROM orders o
+JOIN customers c ON c.customer_id = o.customer_id
+ORDER BY o.customer_id;
+
+
+USE sql_store;
+SELECT o.customer_id, o.order_id, c.first_name, c.last_name FROM orders o -- o = alias for orders 
+JOIN customers c USING (customer_id) -- USING izmanto kad abas tabulas ir vienads lauku nosaukums
+ORDER BY o.customer_id;
+
+
